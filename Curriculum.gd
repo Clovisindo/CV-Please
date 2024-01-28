ac7c47455afaf6dd9f631edf87b4bfa7dd11c4d7
Clovis <kiliranshe@gmail.com>
2024-01-28 19:51:39 +0100

    Escena CV:añadir boton y evento
    Añadimos boton con evento para una habilidad

---
+++ Curriculum.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _init():
+	print("iniciar")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_RichTextLabel_meta_clicked(meta):
+	print("test")
+
+
+
+func _on_Button_pressed():
+	print("test")
