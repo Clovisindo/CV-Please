2935264802dbd50c754c321f3bb435519d6d597a
Clovis <kiliranshe@gmail.com>
2024-02-01 20:31:55 +0100

    Panel dinamico y instancia por codigo de lineas
    El control VBoxContainer adapta dinamicamente el tamaño segun los objetos que entran( aunque esta no es la configuracion que queremos, será parecida).
    Creada clase para SkillPanel, al querer instanciar las lineas por codigo, tenemos que poner un script para crear el signal cuando se instancian.
    Tambien se ha incluido un metodo en Curriculum.gd para instanciar los paneles con los datos leidos, de momento se meten a machete en codigo

--- Curriculum.gd
+++ Curriculum.gd
@@ -1,16 +1,35 @@
 extends Control
 
+export(PackedScene) var skill_Scene
+
 var MouseOver = false
 var eventFired = false
+var skill_string_1 = "skill 1 loaded"
+var skill_string_2 = "skill 2 loaded"
 
 func _init():
 	print("Init")
+
+func _ready():
+	var CVPanel = get_node("CVPanel/VBoxContainer")
+	instanceNewSkillLine("skill 1","asnwer 1",CVPanel)
+	instanceNewSkillLine("skill 2","asnwer 2",CVPanel)
+	instanceNewSkillLine("skill 3","asnwer 3",CVPanel)
+
 
 func _on_Panel_gui_input(event):
 	if event is InputEventMouseButton:
 		if MouseOver == true && eventFired == false:
 			print("Answer 1")
 			eventFired = true
+
+func instanceNewSkillLine(skill_name, skill_answer, CVPanel):
+	var skillPanel = skill_Scene.instance()
+	skillPanel.skill_text = skill_name
+	skillPanel.skill_answer = skill_answer
+	skillPanel.get_node("SkillText").text = skillPanel.skill_text
+	CVPanel.add_child(skillPanel)
+	
 
 func _on_Panel_mouse_entered():
 	MouseOver = true
---
+++ SkillPanel.gd
@@ -0,0 +1,10 @@
+extends Panel
+
+var skill_text
+var skill_answer
+
+func _ready():
+	var control = get_tree().root.get_child(0)
+	self.connect("mouse_entered",control,"_on_Panel_mouse_entered")
+	self.connect("mouse_exited",control,"_on_Panel_mouse_exited")
+	self.connect("gui_input",control,"_on_Panel_gui_input")
