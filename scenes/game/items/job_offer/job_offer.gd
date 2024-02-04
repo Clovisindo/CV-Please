e724822369f6f5db1afa84776e6269c0ef230b3d
Clovis <kiliranshe@gmail.com>
2024-02-04 13:24:24 +0100

    Creamos escena job offer
    Escena identica a la del curriculum, por el momento no tiene funcionalidades distintas

---
+++ scenes/game/items/job_offer/job_offer.gd
@@ -0,0 +1,39 @@
+extends Control
+
+export(PackedScene) var requisiteScene
+
+var mouse_over = false
+var event_fired = false
+var requisite_string_1 = "requisite 1 loaded"
+var requisite_string_2 = "requisite 2 loaded"
+
+func _init():
+	print("Init")
+
+func _ready():
+	var jobOfferPanel = get_node("JobOfferPanel/VBoxContainer")
+	instance_new_requisite_line("requisite 1","asnwer 1",jobOfferPanel)
+	instance_new_requisite_line("requisite 2","asnwer 2",jobOfferPanel)
+	instance_new_requisite_line("requisite 3","asnwer 3",jobOfferPanel)
+
+
+func _on_Panel_gui_input(event,requisite_answer):
+	if event is InputEventMouseButton:
+		if mouse_over == true && event_fired == false:
+			print(requisite_answer)
+			event_fired = true
+
+func instance_new_requisite_line(requisite_name, requisite_answer, jobOfferPanel):
+	var requisitePanel = requisiteScene.instance()
+	requisitePanel.requisite_text = requisite_name
+	requisitePanel.requisite_answer = requisite_answer
+	requisitePanel.get_node("RequisiteText").text = requisitePanel.requisite_text
+	jobOfferPanel.add_child(requisitePanel)
+	
+
+func _on_Panel_mouse_entered():
+	mouse_over = true
+
+func _on_Panel_mouse_exited():
+	mouse_over = false
+	event_fired = false
