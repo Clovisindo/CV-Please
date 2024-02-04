5588d720b7df007b796a350d50d210681033965e
Clovis <kiliranshe@gmail.com>
2024-02-04 14:01:04 +0100

    escena y clase applicant
    Escena con una imagen y un texto para cargar informacion del entrevistado.

---
+++ scenes/game/characters/applicant.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+export (Texture) var portrait 
+var appl_name
+
+func _ready():
+	_set_applicant_data(portrait,"applicant 1")
+
+func _set_applicant_data( texture, name):
+	$Portrait.texture = texture
+	$Name.set_bbcode("[center]%s[/center]" % name)
