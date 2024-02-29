fd05f1139a9df0cff0a28e146701c37445695611
Clovis <kiliranshe@gmail.com>
2024-02-29 16:58:27 +0100

    Correcion nombres parametros resources
    Mejores nombres a las variables de resources nuevas.

--- src/domain/resources/resource_dummy_responses.gd
+++ src/domain/resources/resource_dummy_responses.gd
@@ -1,9 +1,9 @@
 extends Resource
 class_name ResourceDummyResponse
 
-export var textUI: String
-export var response: String
+export var response1: String
+export var response2: String
 
-func _init( t:String, r:String):
-	self.textUI = t
-	self.response = r
+func _init( r1:String, r2:String):
+	self.response1 = r1
+	self.response2 = r2
