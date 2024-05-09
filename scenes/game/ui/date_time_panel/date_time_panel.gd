3c35a20ec96ab035cf5e8d0c68f7796baf890362
Clovis <kiliranshe@gmail.com>
2024-05-09 18:29:41 +0200

    70 hacer visible el paso del tiempo (#73)
    * Nuevo panel calendario y hora
    
    Funcionalidad de contar los dias y lashoras pasando con cada candidato, si te pasas de los turnos asignados para hacer "rapido" al candidato, suma algunos minutos mas.
    
    * refactorizacion
    
    Formato y refactorizacion codigo
    
    * Revision PR enum meses
    
    Eliminamos el enum personalizado y usamos el facilitado por godot
    
    * Cambio nombre escenas y scripts a snake_case

---
+++ scenes/game/ui/date_time_panel/date_time_panel.gd
@@ -0,0 +1,66 @@
+extends Panel
+
+class_name DateTimePanel
+
+const DATE_INITIAL = "January"
+const TIME_INITIAL = "08:00"
+
+var current_hours = 8
+var current_minutes = 0
+var timer: Timer
+
+
+func _ready() -> void:
+	$MonthLabel.text = DATE_INITIAL
+	$TimeLabel.text = TIME_INITIAL
+	_set_text_hour(current_hours, current_minutes)
+
+
+func _set_text_hour(hour_value, minutes_value):
+	var text_hour
+	var text_minutes
+
+	if minutes_value + current_minutes > 60:
+		current_minutes = (minutes_value + current_minutes) - 60
+	if current_minutes < 10:
+		text_minutes = "0" + String(current_minutes)
+	else:
+		text_minutes = String(current_minutes)
+
+	if hour_value < 10:
+		text_hour = "0" + String(current_hours)
+	else:
+		text_hour = String(current_hours)
+
+	$TimeLabel.text = text_hour + ":" + text_minutes
+
+
+func set_current_month(_ecurrent_month):
+	var current_month = _get_month_value(_ecurrent_month)
+	$MonthLabel.text = current_month
+
+
+func set_datetime_by_validation(turns_value_validate):
+	if turns_value_validate:
+		current_hours += 1
+		_set_text_hour(current_hours, current_minutes)
+	else:
+		current_hours += 1
+		current_minutes += 40
+		_set_text_hour(current_hours, current_minutes)
+
+
+func _get_month_value(enum_month):
+	match enum_month:
+		Time.MONTH_JANUARY:
+			return "January"
+		Time.MONTH_FEBRUARY:
+			return "February"
+		Time.MONTH_MARCH:
+			return "March"
+		Time.MONTH_APRIL:
+			return "April"
+		Time.MONTH_MAY:
+			return "May"
+		Time.MONTH_JUNE:
+			return "June"
--- scenes/main/game_manager.gd
+++ scenes/main/game_manager.gd
@@ -5,6 +5,10 @@ signal company_alert_message
 signal emit_message_to_player_dialog_box
 signal emit_message_to_applicant_dialog_box
 
+const TURN_VALUE_REQUISITE = 1
+const TURN_VALUE_SKILL = 1
+const TURN_VALUE_CROSS = 2
+
 export(PackedScene) onready var applicant_scene
 export(PackedScene) onready var decision_applicant_scene
 export(PackedScene) onready var interaction_dialog_scene
@@ -18,6 +22,7 @@ var current_interaction_dialog: InteractionDialog
 var computer_interaction_dialog
 var player_dialog_box: DialogBox
 var applicant_dialog_box: DialogBox
+var datetime_panel: DateTimePanel
 
 
 func _ready():
@@ -27,6 +32,8 @@ func _ready():
 	computer_interaction_dialog = $MainScene/ChatLogKeyboard
 	main_computer = $MainScene/MainComputer
 	company_computer = $MainScene/CompanyComputer
+	datetime_panel = $MainScene/DateTimePanel
+	datetime_panel.set_current_month(Global.current_month)
 	_instantiate_panels()
 	_wire_events()
 	_load_next_applicant()
@@ -150,6 +157,7 @@ func _on_skill_selected(skill: SkillPanel):
 	# No pasamos parametro por que la actual se va a otro estando distinto por el input
 	applicant_list[current_applicant_index].get_cv().disable_other_skills()
 	applicant_list[current_applicant_index].get_job_offer().disable_other_requisites()
+	applicant_list[current_applicant_index].add_turn_count(TURN_VALUE_SKILL)
 	current_interaction_dialog.add_interaction_line(
 		QuestionAnswer.new(skill.skill_question, skill.skill_answer), skill
 	)
@@ -165,6 +173,7 @@ func _on_skill_selected(skill: SkillPanel):
 func _on_job_requisite_selected(job_requisite: JobRequisite):
 	applicant_list[current_applicant_index].get_job_offer().disable_other_requisites()
 	applicant_list[current_applicant_index].get_cv().disable_other_skills()
+	applicant_list[current_applicant_index].add_turn_count(TURN_VALUE_REQUISITE)
 	current_interaction_dialog.add_interaction_line(
 		QuestionAnswer.new(job_requisite.requisite_question, job_requisite.requisite_answer),
 		job_requisite
@@ -206,7 +215,8 @@ func _instantiate_panels():
 			puzzle.category_job,
 			puzzle.validate_solution,
 			puzzle.payment_salary,
-			puzzle.detail_validations
+			puzzle.detail_validations,
+			puzzle.time_limit
 		)
 		new_applicant.set_positions_applicant(
 			$MainScene/ApplicantContainer/ApplicantEntrancePosition.position,
@@ -268,6 +278,13 @@ func process_validations_applicant(applicant: Applicant):
 				detail.set_value(detail.value_text_ok, detail.value_ok)
 			else:
 				detail.set_value(detail.value_text_nok, detail.value_nok)
+		if detail.type_special_condition == EnumUtils.TypeSpecialCondition.TIME_CHECK:
+			if applicant.turns_count <= applicant.validation_turns:
+				detail.set_value(detail.value_text_ok, detail.value_ok)
+				datetime_panel.set_datetime_by_validation(true)
+			else:
+				detail.set_value(detail.value_text_nok, detail.value_nok)
+				datetime_panel.set_datetime_by_validation(false)
 		applicant.evaluation.details_applicant = applicant.detail_validations
 
 
